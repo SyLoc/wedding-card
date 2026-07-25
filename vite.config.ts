81e75a3e7cd5acfd932792886d99a3bef1a78dc8
Loc Tran <loc.tran@enouvo.com>
2026-07-25 15:47:02 +0700

    Rename page title

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+
+  console.log('BASE:', env.VITE_BASE_URL)
   return {
     plugins: [react()],
     server: {
